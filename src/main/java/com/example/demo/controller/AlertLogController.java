5631c100c0858e77bcaa30c94c1d6461557dfa0c
bala-amypo <bala@amypo.in>
2025-12-18 13:34:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/AlertLogController.java
+++ src/main/java/com/example/demo/controller/AlertLogController.java
@@ -1,25 +1,17 @@
-package com.example.warranty.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.warranty.entity.AlertLog;
-import com.example.warranty.service.AlertLogService;
-
 @RestController
 @RequestMapping("/alert-logs")
 public class AlertLogController {
 
     @Autowired
-    private AlertLogService alertLogService;
+    private AlertLogService service;
 
-    // Get all alert logs
+    @PostMapping
+    public AlertLog add(@RequestBody AlertLog alertLog) {
+        return service.save(alertLog);
+    }
+
     @GetMapping
-    public List<AlertLog> getAllAlertLogs() {
-        return alertLogService.getAll();
+    public List<AlertLog> getAll() {
+        return service.getAll();
     }
 }
