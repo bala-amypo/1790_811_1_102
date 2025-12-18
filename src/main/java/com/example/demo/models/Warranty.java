ca9e10a98a7463eba52fe5fc7dea8c2f0166f64d
bala-amypo <bala@amypo.in>
2025-12-18 14:01:26 +0000

    User commit

--- src/main/java/com/example/demo/models/Warranty.java
+++ src/main/java/com/example/demo/models/Warranty.java
@@ -1,10 +1,3 @@
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 public class Warranty {
