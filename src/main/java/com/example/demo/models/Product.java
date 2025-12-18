d1c886437dfbb8bb5b2e31a7ebfab861b3bb7ce6
bala-amypo <bala@amypo.in>
2025-12-18 12:46:24 +0000

    User commit

--- src/main/java/com/example/demo/models/Product.java
+++ src/main/java/com/example/demo/models/Product.java
@@ -1,8 +1,64 @@
-package com.example
+@Entity
+public class Product {
 
-public Loge getId() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String brand;
+    private String category;
+    private String serialNumber;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
     return id;
 }
-public void setId(Long id){
-    this id = id;
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getBrand() {
+    return brand;
+}
+
+public void setBrand(String brand) {
+    this.brand = brand;
+}
+
+public String getCategory() {
+    return category;
+}
+
+public void setCategory(String category) {
+    this.category = category;
+}
+
+public String getSerialNumber() {
+    return serialNumber;
+}
+
+public void setSerialNumber(String serialNumber) {
+    this.serialNumber = serialNumber;
+}
+
+public User getUser() {
+    return user;
+}
+
+public void setUser(User user) {
+    this.user = user;
+}
+
 }
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,42 +1,56 @@
-package com.example.demo.models;
-
-import jakarta.persistence.*;
-import lombok.Data;
-import java.util.list;
-
 @Entity
 @Table(name = "users")
-public class User{
+public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String email;
-    private String password;
 
-    public Long getId(){
-        return id;
-    }
-     public String getUsername(){
-        return username;
-    }
-      public String getEmail(){
-        return Email;
-    }
-     public String getPassword(){
-        return Password;
-    }
-    public void setId(Long id;){
-        this.id = id;
-    }
-    public void setUsername(String name;){
-        this.Name = name;
-    }
-    public void setEmail(String email;){`
-        this.Email = email;
-    }
-    public void setPassword(Long password;){
-        this.Password = password;
-    }
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String name;
+    private String password;
+    private String role; 
+
+    public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getPassword() {
+    return password;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+
+public String getRole() {
+    return role;
+}
+
+public void setRole(String role) {
+    this.role = role;
+}
 
 }
