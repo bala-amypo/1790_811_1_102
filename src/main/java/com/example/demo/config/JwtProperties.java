f0460846fca3350b36db9d19ab5c50fb79b07e08
bala-amypo <bala@amypo.in>
2025-12-22 03:45:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/JwtProperties.java
@@ -0,0 +1,35 @@
+package com.example.demo.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    /**
+     * Secret key used to sign JWT
+     */
+    private String secret;
+
+    /**
+     * Token expiration time in milliseconds
+     */
+    private long expiration;
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public long getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(long expiration) {
+        this.expiration = expiration;
+    }
+}
