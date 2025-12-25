61e22ca550bef2cfa8c5adf05e495387f9dfa026
bala-amypo <bala@amypo.in>
2025-12-25 15:22:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,17 +1,24 @@
 package com.example.demo.entity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.List;
 
 @Entity
 @Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     private String brand;
@@ -23,35 +30,5 @@ public class Product {
     private String category;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
-    @JsonIgnore
     private List<Warranty> warranties;
-
-    public Product() {}
-
-    public Product(Long id, String name, String brand, String modelNumber, String category, List<Warranty> warranties) {
-        this.id = id;
-        this.name = name;
-        this.brand = brand;
-        this.modelNumber = modelNumber;
-        this.category = category;
-        this.warranties = warranties;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getBrand() { return brand; }
-    public void setBrand(String brand) { this.brand = brand; }
-
-    public String getModelNumber() { return modelNumber; }
-    public void setModelNumber(String modelNumber) { this.modelNumber = modelNumber; }
-
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
-
-    public List<Warranty> getWarranties() { return warranties; }
-    public void setWarranties(List<Warranty> warranties) { this.warranties = warranties; }
 }
