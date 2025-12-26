937bb3a0a06f3dee39f72a99fde3f924b67ad74a
bala-amypo <bala@amypo.in>
2025-12-26 15:09:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/WarrantyRepository.java
+++ src/main/java/com/example/demo/repository/WarrantyRepository.java
@@ -1,13 +1,16 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Warranty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Warranty;
+
+@Repository
 public interface WarrantyRepository extends JpaRepository<Warranty, Long> {
 
     boolean existsBySerialNumber(String serialNumber);
 
-    List<Warranty> findByUserId(Long userId);  // ✅ REQUIRED
+    List<Warranty> findByUser_Id(Long userId);
 }
